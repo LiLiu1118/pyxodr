fde0ef5e619dcf179c8b46e73a8161a185dbe7e8
li <li0121582@gmail.com>
2022-10-19 21:13:18 +0200

    updated the test script

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ from xodr_parser import XODRParser
 
 path = "example_files/2022-09-13_1579_AHEAD_Lippstadt_Innenstadt_HeWeg_ODR.xodr"
 parser1 = XODRParser(path)
-parser1.set_id("1002000")
-parser1.set_get_ref_line_params()
-parser1.plot_ref_line()
+
+road = parser1.find_road("1002000")
+
+road.calculate_ref_line_params()
+road.plot_ref_line()
