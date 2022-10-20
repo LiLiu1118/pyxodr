08311a87f3859a864adac483084ee5e98bbb2a51
li <li0121582@gmail.com>
2022-10-20 21:27:43 +0200

    add elevationProfile.py

---
+++ elevationProfile.py
@@ -0,0 +1,12 @@
+
+class ElevationProfile:
+    def __init__(self):
+        self.elevations = []
+
+class Elevation:
+    def __init__(self, s, a, b, c, d):
+        self.s = s
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
