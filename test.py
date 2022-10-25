2f8ee51d2d4efb1075347a08b61b666aa7e62a40
li <li0121582@gmail.com>
2022-10-25 21:21:23 +0200

    update test.py

--- test.py
+++ test.py
@@ -1,11 +1,27 @@
 from xodr_parser import XODRParser
+import matplotlib.pyplot as plt
+import numpy as np
+import time
 
 path = "example_files/2022-09-13_1579_AHEAD_Lippstadt_Innenstadt_HeWeg_ODR.xodr"
 parser1 = XODRParser(path)
 
+time1 = time.time()
 road_parser = parser1.find_road("1002000")
+time2 = time.time()
+print("Road found: Time used --- %s seconds ---" % (time2 - time1))
 
-road_parser.setup_road_information()
-road_parser.plot_ref_line()
+time3 = time.time()
+road_parser.parse_geometry_parameters()
+time4 = time.time()
+print("Parameter parsed: Time used --- %s seconds ---" % (time4 - time3))
+
+time5 = time.time()
+road_parser.calculate_3d_coordinate()
+time6 = time.time()
+print("All coordinates calculated: Time used --- %s seconds ---" % (time6 - time5))
+
+road_parser.plot_2D()
+
 
 
