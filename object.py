87c01242b1f75ce3a958bfcb30275c8e0794a5d7
li <li0121582@gmail.com>
2023-01-23 01:12:26 +0100

    files deleted

--- object.py
+++
@@ -1,8 +0,0 @@
-
-class Object:
-    def __init__(self, object_type, object_subtype, object_id, s, t):
-        self.type = object_type
-        self.subtype = object_subtype
-        self.id = object_id
-        self.s = s
-        self.t = t
